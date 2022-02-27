676399812eb7a097f49f240869e4a843f9701721
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-02-27 18:15:36 +0530

    added products

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const products = [
+    {
+        id: '1',
+        name: 'Macbook',
+        description: 'Apple Macbook Pro M1',
+        price: '₹1,95000.00',
+        image: 'https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        id: '2',
+        name: 'Iphone 13',
+        description: 'Apple Iphone 13 Pro Max',
+        price: '₹1,15000.00',
+        image: 'https://images.pexels.com/photos/9667337/pexels-photo-9667337.jpeg?cs=srgb&dl=pexels-caleb-oquendo-9667337.jpg&fm=jpg'
+    },
+    {
+        id: '3',
+        name: 'Surface',
+        description: 'Microsoft Surface Pro 8',
+        price: '₹1,25000.00',
+        image: 'https://images.unsplash.com/photo-1617780421536-f74c55b953ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
+    },
+    {
+        id: '4',
+        name: 'iWatch',
+        description: 'Apple Watch Series 7',
+        price: '₹35,000.00',
+        image: 'https://images.unsplash.com/photo-1617043786394-f977fa12eddf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        id: '5',
+        name: 'Earbuds',
+        description: 'Sony WF-1000XM4 Noice Cancelling',
+        price: '₹20,000.00',
+        image: 'https://images.unsplash.com/photo-1631176093617-63490a3d785a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+    },
+    {
+        id: '6',
+        name: 'Nikon Camera',
+        description: 'Nikon D850 DSLR',
+        price: '₹2,00000.00',
+        image: 'https://images.unsplash.com/photo-1624913503273-5f9c4e980dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        id: '7',
+        name: 'NVIDIA Gpu',
+        description: 'Nvidia GTX 2080',
+        price: '₹1,50000.00',
+        image: 'https://images.unsplash.com/photo-1591405351990-4726e331f141?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        id: '8',
+        name: 'Headphones',
+        description: 'Razer Black Shark V2',
+        price: '₹15000.00',
+        image: 'https://images.unsplash.com/photo-1636487658544-7704a1fad45e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+    },
+
+]
