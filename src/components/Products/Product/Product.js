2d6149e93d4e08a552e333013ee27dbeebd9fa08
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-02-27 18:16:53 +0530

    added Products and Product component

---
+++ src/components/Products/Product/Product.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, Button } from '@material-ui/core';
+
+import useStyles from './styles';
+import { Context } from '../../../Context/Context';
+
+const Product = ({ product }) => {
+  const classes = useStyles();
+  const { addToCart } = useContext(Context);
+
+  
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.price}
+          </Typography>
+        </div>
+        <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <Button variant='contained' color='primary' onClick={() => addToCart(product)}>Add to cart</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product
