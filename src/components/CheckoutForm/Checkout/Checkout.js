4f6ecb8b304c3d80fad5f0c56f9feb365a37d868
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-02-28 15:49:28 +0530

    added CheckoutForm component

---
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+import OrderSummary from '../OrderSummary';
+import { CssBaseline, Typography, Paper, Stepper, Step, StepLabel, Button } from '@material-ui/core';
+import useStyles from './styles'
+
+
+const steps = ['Shipping address', 'Payment details', 'Order Summary'];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    case 2:
+      return <OrderSummary />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
+
+const Checkout = () => {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+  return (
+    <>
+      <CssBaseline />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <>
+            {activeStep === steps.length ? (
+              <>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order confirmation, and will
+                  send you an update when your order has shipped.
+                </Typography>
+              </>
+            ) : (
+              <>
+                {getStepContent(activeStep)}
+                <div className={classes.buttons}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} className={classes.button}>
+                      Back
+                    </Button>
+                  )}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                  </Button>
+                </div>
+              </>
+            )}
+          </>
+        </Paper>
+      </main>
+    </>
+  )
+}
+
+export default Checkout
